28a903719c390429860faeb8ca7c1ae4a67bae03
Tony Butler <tonyjbutler@gmail.com>
2014-04-09 16:50:35 +0100

    Increment build number for 2.5.1 release
    Signed-off-by: Tony Butler <tonyjbutler@gmail.com>

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2014040302;       // The current module version (Date: YYYYMMDDXX)
+$module->version   = 2014040900;       // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2013051400;       // Requires this Moodle version
 $module->component = 'mod_aspirelist'; // Full name of the plugin (used for diagnostics)
 $module->cron      = 0;
-$module->release   = '2.5.0';
+$module->release   = '2.5.1';
 $module->maturity  = MATURITY_STABLE;
