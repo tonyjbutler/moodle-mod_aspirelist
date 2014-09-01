bb00b45fe0fadf0ebd46e896b2f2d335f8c87cf4
Tony Butler <tonyjbutler@gmail.com>
2014-09-01 10:26:48 +0100

    Update version information for beta testing
    Signed-off-by: Tony Butler <tonyjbutler@gmail.com>

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014080303;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2014090100;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;       // Requires this Moodle version
 $plugin->component = 'mod_aspirelist'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->maturity  = MATURITY_BETA;
