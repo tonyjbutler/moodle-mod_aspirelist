f5c9378f6df5777afe7170f24f9c1f99f91f54b9
Tony Butler <tonyjbutler@gmail.com>
2015-01-21 15:44:37 +0000

    Update version.php for 2.7.3 release

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014103002;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2015012102;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;       // Requires this Moodle version
 $plugin->component = 'mod_aspirelist'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
-$plugin->release   = '2.7.2';
+$plugin->release   = '2.7.3';
 $plugin->maturity  = MATURITY_STABLE;
