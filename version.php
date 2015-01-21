67d7cc7b50bbf8ad6cd78db2053c37f5e46b4499
Tony Butler <tonyjbutler@gmail.com>
2015-01-21 15:46:31 +0000

    Update version.php to reflect stable releases

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014103003;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2015012103;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;       // Requires this Moodle version
 $plugin->component = 'mod_aspirelist'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
