b2e8a18a8760a17aeeda174e390e8ba8f243617b
Tony Butler <tonyjbutler@gmail.com>
2016-05-04 14:23:31 +0100

    mod_aspirelist: Refactor and reorganise admin config settings form

--- settings.php
+++ settings.php
@@ -28,28 +28,31 @@ defined('MOODLE_INTERNAL') || die;
 require_once($CFG->dirroot . '/mod/aspirelist/adminlib.php');
 
 if ($ADMIN->fulltree) {
-    // General settings.
-    $settings->add(new admin_setting_configcheckbox('aspirelist/requiremodintro', get_string('requiremodintro', 'aspirelist'),
-            get_string('configrequiremodintro', 'aspirelist'), 0));
 
-    // Authors in module config form.
-    $settings->add(new admin_setting_configcheckbox('aspirelist/authorsinconfig', get_string('authorsinconfig', 'aspirelist'),
-            get_string('configauthorsinconfig', 'aspirelist'), 0));
+    // General settings.
+    $settings->add(new admin_setting_heading('aspirelist/general', get_string('generalsettings', 'aspirelist'), ''));
+    $settings->add(new admin_setting_configcheckbox('aspirelist/requiremodintro', get_string('requiremodintro', 'aspirelist'),
+            get_string('requiremodintro_desc', 'aspirelist'), 0));
 
     // Display settings.
     $optionsdd = array();
     $optionsdd[0] = get_string('displaypage', 'aspirelist');
     $optionsdd[1] = get_string('displayinline', 'aspirelist');
     $settings->add(new admin_setting_configselect('aspirelist/defaultdisplay', get_string('defaultdisplay', 'aspirelist'),
-            get_string('configdefaultdisplay', 'aspirelist'), 'displaypage', $optionsdd));
+            get_string('defaultdisplay_desc', 'aspirelist'), 'displaypage', $optionsdd));
 
-    // Aspire URL.
+    // Authors in module config form.
+    $settings->add(new admin_setting_configcheckbox('aspirelist/authorsinconfig', get_string('authorsinconfig', 'aspirelist'),
+            get_string('authorsinconfig_desc', 'aspirelist'), 0));
+
+    // Talis Aspire site settings.
+    $settings->add(new admin_setting_heading('aspirelist/aspiresite', get_string('aspiresitesettings', 'aspirelist'), ''));
+
+    // Aspire URL and HTTPS alias.
     $settings->add(new admin_setting_configtext('aspirelist/aspireurl', get_string('aspireurl', 'aspirelist'),
-            get_string('configaspireurl', 'aspirelist'), 'http://', PARAM_URL));
-
-    // Aspire URL HTTPS alias.
+            get_string('aspireurl_desc', 'aspirelist'), 'http://', PARAM_URL));
     $settings->add(new admin_setting_configtext('aspirelist/aspireurlhttpsalias', get_string('aspireurlhttpsalias', 'aspirelist'),
-            get_string('configaspireurlhttpsalias', 'aspirelist'), 'https://', PARAM_URL));
+            get_string('aspireurlhttpsalias_desc', 'aspirelist'), 'https://', PARAM_URL));
 
     // Knowledge group.
     $optionskg = array();
@@ -59,7 +62,10 @@ if ($ADMIN->fulltree) {
     $optionskg['programmes'] = get_string('programmes', 'aspirelist');
     $optionskg['subjects'] = get_string('subjects', 'aspirelist');
     $settings->add(new admin_setting_configselect('aspirelist/knowledgegroup', get_string('knowledgegroup', 'aspirelist'),
-            get_string('configknowledgegroup', 'aspirelist'), 'modules', $optionskg));
+            get_string('knowledgegroup_desc', 'aspirelist'), 'modules', $optionskg));
+
+    // Aspire code settings.
+    $settings->add(new admin_setting_heading('aspirelist/aspirecodes', get_string('aspirecodesettings', 'aspirelist'), ''));
 
     // Code source.
     $optionscs = array();
@@ -67,25 +73,26 @@ if ($ADMIN->fulltree) {
     $optionscs['shortname'] = get_string('shortnamecourse');
     $optionscs['codetable'] = get_string('codetable', 'aspirelist');
     $settings->add(new aspirelist_codesource_setting('aspirelist/codesource', get_string('codesource', 'aspirelist'),
-            get_string('configcodesource', 'aspirelist'), 'idnumber', $optionscs));
-
-    // Meta child codes.
-    $settings->add(new admin_setting_configcheckbox('aspirelist/includechildcodes', get_string('includechildcodes', 'aspirelist'),
-            get_string('configincludechildcodes', 'aspirelist'), 0));
+            get_string('codesource_desc', 'aspirelist'), 'idnumber', $optionscs));
 
     // Code regexes.
     $settings->add(new admin_setting_configtext('aspirelist/coderegex', get_string('coderegex', 'aspirelist'),
-            get_string('configcoderegex', 'aspirelist'), '', PARAM_TEXT));
+            get_string('coderegex_desc', 'aspirelist'), '', PARAM_TEXT));
     $settings->add(new admin_setting_configtext('aspirelist/yearregex', get_string('yearregex', 'aspirelist'),
-            get_string('configyearregex', 'aspirelist'), '', PARAM_TEXT));
+            get_string('yearregex_desc', 'aspirelist'), '', PARAM_TEXT));
 
     // Code table details.
     $settings->add(new aspirelist_codetable_setting('aspirelist/codetable', get_string('codetable', 'aspirelist'),
-            get_string('configcodetable', 'aspirelist'), '', PARAM_TEXT));
+            get_string('codetable_desc', 'aspirelist'), '', PARAM_TEXT));
     $settings->add(new aspirelist_codecolumn_setting('aspirelist/codecolumn', get_string('codecolumn', 'aspirelist'),
-            get_string('configcodecolumn', 'aspirelist'), '', PARAM_TEXT));
+            get_string('codecolumn_desc', 'aspirelist'), '', PARAM_TEXT));
     $settings->add(new aspirelist_coursecolumn_setting('aspirelist/coursecolumn', get_string('coursecolumn', 'aspirelist'),
-            get_string('configcoursecolumn', 'aspirelist'), '', PARAM_TEXT));
+            get_string('coursecolumn_desc', 'aspirelist'), '', PARAM_TEXT));
     $settings->add(new aspirelist_courseattribute_setting('aspirelist/courseattribute',
-            get_string('courseattribute', 'aspirelist'), get_string('configcourseattribute', 'aspirelist'), '', PARAM_TEXT));
+            get_string('courseattribute', 'aspirelist'), get_string('courseattribute_desc', 'aspirelist'), '', PARAM_TEXT));
+
+    // Meta child codes.
+    $settings->add(new admin_setting_configcheckbox('aspirelist/includechildcodes', get_string('includechildcodes', 'aspirelist'),
+            get_string('includechildcodes_desc', 'aspirelist'), 0));
+
 }
