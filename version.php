a6a185688ca27d314937e47cb63b0fdc533dc31e
Tony Butler <tonyjbutler@gmail.com>
2016-05-18 10:38:23 +0100

    mod_aspirelist: Bump version to invoke a rescan of cache definitions

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015052100;       // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2016051800;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015051100;       // Requires this Moodle version.
 $plugin->component = 'mod_aspirelist'; // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
